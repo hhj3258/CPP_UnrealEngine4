b2e9c07416be473e996c584e15b1260b7522fb65
Hong Hyeon-Jun <70702088+hhj3258@users.noreply.github.com>
2022-05-26 15:53:22 +0900

    Chapter03 : 애니메이션 몽타주

---
+++ 03_Animation/c_Animation_Montage/Source/a_Unity_VS_UE/MyAnimInstance.cpp
@@ -0,0 +1,60 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyAnimInstance.h"
+
+#include "GameFramework/Character.h"
+#include "GameFramework/PawnMovementComponent.h"
+
+void UMyAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+
+	if (IsValid(Pawn))
+	{
+		//Speed = Pawn->GetVelocity().Size();
+
+		const auto Character = Cast<ACharacter>(Pawn);
+
+		// W = 1, S = -1, A = -1, D = 1
+		UpDownSpeed = Character->GetInputAxisValue(TEXT("UpDown"));
+		LeftRightSpeed = Character->GetInputAxisValue(TEXT("LeftRight"));
+
+		//UE_LOG(LogTemp,Log,TEXT("speed : %f"), Speed);
+
+		if (IsValid(Character))
+		{
+			IsFalling = Character->GetMovementComponent()->IsFalling();
+		}
+	}
+}
+
+void UMyAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+	Pawn = TryGetPawnOwner();
+}
+
+// 생성자
+UMyAnimInstance::UMyAnimInstance()
+{
+	// 몽타주 경로를 통해서 검색해서 찾아온다.
+	static ConstructorHelpers::FObjectFinder<UAnimMontage> AM(
+		TEXT("AnimMontage'/Game/Animations/Greystone_Skeleton_Montage.Greystone_Skeleton_Montage'"));
+
+	// 정상적으로 찾았으면
+	if (AM.Succeeded())
+	{
+		// 몽타주 변수에 할당
+		AttackMontage = AM.Object;
+	}
+}
+
+void UMyAnimInstance::PlayAttackMontage()
+{
+	if (!Montage_IsPlaying(AttackMontage))
+	{
+		// Montage_Play(플레이할 몽타주, 재생 속도) 
+		Montage_Play(AttackMontage, 1.f);
+	}
+}
